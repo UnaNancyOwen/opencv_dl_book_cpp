affb21ce66a7829c31de24c0934fa3f666cf27da
Tsukasa Sugiura <t.sugiura0204@gmail.com>
2022-12-31 19:36:38 +0900

    Add OpenCV Face Detector

---
+++ ch7/7.2/opencv_face_detector/README.md
@@ -0,0 +1,16 @@
+# OpenCV Face Detector
+
+## モデル
+
+* [OpenCV Face Detector](https://github.com/opencv/opencv/tree/4.5.5/samples/dnn#face-detection)  
+  * [opencv_face_detector.caffemodel](https://github.com/opencv/opencv_3rdparty/raw/dnn_samples_face_detector_20170830/res10_300x300_ssd_iter_140000.caffemodel)  
+  * [opencv_face_detector.prototxt](https://raw.githubusercontent.com/opencv/opencv/4.5.5/samples/dnn/face_detector/deploy.prototxt)  
+  * [opencv_face_detector_fp16.caffemodel](https://github.com/opencv/opencv_3rdparty/raw/19512576c112aa2c7b6328cb0e8d589a4a90a26d/res10_300x300_ssd_iter_140000_fp16.caffemodel)  
+  * [opencv_face_detector_fp16.prototxt](https://raw.githubusercontent.com/opencv/opencv/4.5.5/samples/dnn/face_detector/deploy.prototxt)  
+  * [opencv_face_detector_uint8.pb](https://raw.githubusercontent.com/opencv/opencv_3rdparty/dnn_samples_face_detector_20180220_uint8/opencv_face_detector_uint8.pb)  
+  * [opencv_face_detector_uint8.pbtxt](https://raw.githubusercontent.com/opencv/opencv/4.5.5/samples/dnn/face_detector/opencv_face_detector.pbtxt)  
+
+## 著作権/ライセンス
+
+* Copyright (c) 2015-2021 OpenCV Foundation  
+* [Apache License Version 2.0](https://github.com/opencv/opencv/blob/4.5.5/LICENSE)  
---
+++ ch7/7.2/opencv_face_detector/opencv_face_detector.cpp
@@ -0,0 +1,79 @@
+#include <vector>
+#include <string>
+#include <stdexcept>
+#include <opencv2/opencv.hpp>
+
+int main(int argc, char* argv[])
+{
+    // キャプチャを開く
+    cv::VideoCapture capture = cv::VideoCapture( "../face.jpg" ); // 画像ファイル
+    //cv::VideoCapture capture = cv::VideoCapture( 0 ); // カメラ
+    if( !capture.isOpened() ){
+        throw std::runtime_error( "can't open capture!" );
+    }
+
+    // モデルを読み込む
+    // const std::string weights = "../opencv_face_detector.caffemodel"; // float32
+    // const std::string config = "../opencv_face_detector.prototxt";
+    const std::string weights = "../opencv_face_detector_fp16.caffemodel"; // float16
+    const std::string config = "../opencv_face_detector_fp16.prototxt";
+    // const std::string weights = "../opencv_face_detector_uint8.pb"; // uint8
+    // const std::string config = "../opencv_face_detector_uint8.pbtxt";
+    cv::dnn::DetectionModel model = cv::dnn::DetectionModel( weights, config );
+
+    // モデルの推論に使用するエンジンとデバイスを設定する
+    model.setPreferableBackend( cv::dnn::DNN_BACKEND_OPENCV );
+    model.setPreferableTarget( cv::dnn::DNN_TARGET_CPU );
+
+    // モデルの入力パラメーターを設定する
+    const double scale = 1.0;                                  // スケールファクター
+    const cv::Size size = cv::Size( 300, 300 );                // 入力サイズ
+    const cv::Scalar mean = cv::Scalar( 104.0, 177.0, 123.0 ); //差し引かれる平均値
+    const bool swap = false;                                   // チャンネルの順番（True: RGB、False: BGR）
+    const bool crop = false;                                   // クロップ
+    model.setInputParams( scale, size, mean, swap, crop );
+
+    while( true ){
+        // フレームをキャプチャして画像を読み込む
+        cv::Mat image;
+        capture >> image;
+        if( image.empty() ){
+            cv::waitKey( 0 );
+            break;
+        }
+
+        // 画像が3チャンネル以外の場合は3チャンネルに変換する
+        if( image.channels() == 1 ){
+            cv::cvtColor( image, image, cv::COLOR_GRAY2BGR );
+        }
+        if( image.channels() == 4 ){
+            cv::cvtColor( image, image, cv::COLOR_BGRA2BGR );
+        }
+
+        // 顔を検出する
+        const float confidence_threshold = 0.6;
+        const float nms_threshold = 0.4;
+        std::vector<int32_t> class_ids;
+        std::vector<float> confidences;
+        std::vector<cv::Rect> boxes;
+        model.detect( image, class_ids, confidences, boxes, confidence_threshold, nms_threshold );
+
+        // 検出した顔のバウンディングボックスを描画する
+        const cv::Scalar color = cv::Scalar( 0, 0, 255 );
+        const int32_t thickness = 2;
+        for( const cv::Rect& box : boxes ){
+            cv::rectangle( image, box, color, thickness, cv::LINE_AA );
+        }
+
+        // 画像を表示する
+        cv::imshow( "face detection", image );
+        const int32_t key = cv::waitKey( 10 );
+        if( key == 'q' ){
+            break;
+        }
+    }
+
+    cv::destroyAllWindows();
+
+    return 0;
+}
